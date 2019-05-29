5ab65bbab26fb285ec404e8b0cca8edac5c4211e
dynnah <dynnah_hanna@hotmail.com>
2019-05-29 08:23:38 -0300

    Update backup.sql

--- database/backup.sql
+++ database/backup.sql
@@ -35,5 +35,5 @@ INSERT INTO egressos (id, nomeCompactado, nome, email, linkedin, github, lattes,
 ('20062370361', 'Aline Donato', 'Aline Donato Carreiro', '', 'https://www.linkedin.com/in/alinedonato/', '', '', '', '', '', '', '', 'cstsi', 'ifpb-jp', 'True', ''),
 ('20052370067', 'Alana Morais', 'Alana Marques de Morais', 'alana_mm@hotmail.com', 'https://www.linkedin.com/in/alana-morais-b6b0a195/', '', '', '', '', '', '', '', 'cstsi', 'ifpb-jp', 'True', ''),
 ('20052370342', 'Alisson Sena', 'Alisson Alcoforado Sena de Lima', 'alisson.sena@gmail.com', 'https://www.linkedin.com/in/alisson-sena/', '', '', '', '', '', '', '', 'cstsi', 'ifpb-jp', 'True', ''),
-('20051370225', 'AndrÃ© Vinagre', 'AndrÃ© Nobre Vinagre', 'andrenvinagre@gmail.com', 'https://www.linkedin.com/in/andre-vinagre/', '', '', '', '', '', '', '', 'cstsi', 'ifpb-jp', 'True', ''),
+('20051370225', 'André Vinagre', 'André Nobre Vinagre', 'andrenvinagre@gmail.com', 'https://www.linkedin.com/in/andre-vinagre/', '', '', '', '', '', '', '', 'cstsi', 'ifpb-jp', 'True', ''),
 ('041037087', 'Andreza Vieira', 'Andreza de Sousa Vieira', 'andreza_sv@yahoo.com.br', 'https://www.linkedin.com/in/andrezavieira/', '', '', '', '', '', '', '', 'cstsi', 'ifpb-jp', 'True', '');
