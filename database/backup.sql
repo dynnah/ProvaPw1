6e6abeba263a12312d32c8a2e4e9fa23ebec546d
dynnah <dynnah_hanna@hotmail.com>
2019-05-29 13:30:38 -0300

    Update backup.sql

--- database/backup.sql
+++ database/backup.sql
@@ -5,7 +5,7 @@ use prova;
 CREATE TABLE usuarios (
     id int auto_increment,
     login varchar(30),
-    senha varchar(30),
+    senha varchar(30)
     PRIMARY KEY(id)
 );
 
@@ -23,16 +23,15 @@ CREATE TABLE egressos (
  );
 
 
-INSERT INTO egressos (id, nomeCompactado, nome, email, linkedin, curso, campus) VALUES
-('20062370361', 'Aline Donato', 'Aline Donato Carreiro',  '', 'https://www.linkedin.com/in/alinedonato/', 'cstsi', 'ifpb-jp');
-('20052370067', 'Alana Morais', 'Alana Marques de Morais',  'alana_mm@hotmail.com', 'https://www.linkedin.com/in/alana-morais-b6b0a195/', 'cstsi', 'ifpb-jp');
-('20052370342', 'Alisson Sena', 'Alisson Alcoforado Sena de Lima', 'alisson.sena@gmail.com', 'https://www.linkedin.com/in/alisson-sena/', 'cstsi', 'ifpb-jp');
-('20051370225', 'André Vinagre', 'André Nobre Vinagre',  'andrenvinagre@gmail.com', 'https://www.linkedin.com/in/andre-vinagre/', 'cstsi', 'ifpb-jp');
-('20041037087', 'Andreza Vieira', 'Andreza de Sousa Vieira', 'andreza_sv@yahoo.com.br', 'https://www.linkedin.com/in/andrezavieira/', 'cstsi', 'ifpb-jp');
-('20061370422', 'Anna Clara Nobrega', 'Anna Clara Rodrigues da Nobrega',  'acrnobrega@hotmail.com','https://www.linkedin.com/in/anna-clara-nobrega/','cstsi','ifpb-jp');
-('20061370040','Andréa Bezerra', 'Andréa Fernanda Fontes Bezerra', 'dsi.andrea@hotmail.com','https://www.linkedin.com/in/andrea-fernanda-478a2660/','cstsi','ifpb-jp');
-('20112370505','Alex Martins','Alex Martins Bezerra', 'alexmbezerrajp@gmail.com','https://www.linkedin.com/in/alex-martins-2b480023/','cstsi','ifpb-jp');
-(20071370107','Ângelo Negreiros', 'Ângelo Lemos Vidal de Negreiros', 'angelolvnegreiros@hotmail','','cstsi','ifpb-jp');
-('20091370072','Alessandra Silva','Alessandra Mendes da Silva', '','','cstsi','ifpb-jp');
-('20042037154','Alline Morais','Alline Silva de Morais', 'allinemorais@gmail.com','https://www.linkedin.com/in/alline-morais-psm-pspo-50002094/','cstsi','ifpb-jp');
+INSERT INTO egressos VALUES ('20062370361', 'Aline Donato', 'Aline Donato Carreiro',  '', 'https://www.linkedin.com/in/alinedonato/', 'cstsi', 'ifpb-jp');
+INSERT INTO egressos VALUES ('20052370067', 'Alana Morais', 'Alana Marques de Morais',  'alana_mm@hotmail.com', 'https://www.linkedin.com/in/alana-morais-b6b0a195/', 'cstsi', 'ifpb-jp');
+INSERT INTO egressos VALUES ('20052370342', 'Alisson Sena', 'Alisson Alcoforado Sena de Lima', 'alisson.sena@gmail.com', 'https://www.linkedin.com/in/alisson-sena/', 'cstsi', 'ifpb-jp');
+INSERT INTO egressos VALUES ('20051370225', 'André Vinagre', 'André Nobre Vinagre',  'andrenvinagre@gmail.com', 'https://www.linkedin.com/in/andre-vinagre/', 'cstsi', 'ifpb-jp');
+INSERT INTO egressos VALUES ('20041037087', 'Andreza Vieira', 'Andreza de Sousa Vieira', 'andreza_sv@yahoo.com.br', 'https://www.linkedin.com/in/andrezavieira/', 'cstsi', 'ifpb-jp');
+INSERT INTO egressos VALUES ('20061370422', 'Anna Clara Nobrega', 'Anna Clara Rodrigues da Nobrega',  'acrnobrega@hotmail.com','https://www.linkedin.com/in/anna-clara-nobrega/','cstsi','ifpb-jp');
+INSERT INTO egressos VALUES ('20061370040','Andréa Bezerra', 'Andréa Fernanda Fontes Bezerra', 'dsi.andrea@hotmail.com','https://www.linkedin.com/in/andrea-fernanda-478a2660/','cstsi','ifpb-jp');
+INSERT INTO egressos VALUES ('20112370505','Alex Martins','Alex Martins Bezerra', 'alexmbezerrajp@gmail.com','https://www.linkedin.com/in/alex-martins-2b480023/','cstsi','ifpb-jp');
+INSERT INTO egressos VALUES ('20071370107','Ângelo Negreiros', 'Ângelo Lemos Vidal de Negreiros', 'angelolvnegreiros@hotmail','','cstsi','ifpb-jp');
+INSERT INTO egressos VALUES ('20091370072','Alessandra Silva','Alessandra Mendes da Silva', '','','cstsi','ifpb-jp');
+INSERT INTO egressos VALUES ('20042037154','Alline Morais','Alline Silva de Morais', 'allinemorais@gmail.com','https://www.linkedin.com/in/alline-morais-psm-pspo-50002094/','cstsi','ifpb-jp');
 
