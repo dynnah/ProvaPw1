9a9035efa6e38c21488d649ef33912279b1358c9
dynnah <dynnah_hanna@hotmail.com>
2019-05-29 10:14:03 -0300

    Update backup.sql

--- database/backup.sql
+++ database/backup.sql
@@ -23,7 +23,7 @@ CREATE TABLE egressos (
  );
 
 
-INSERT INTO egressos (id, nomeCompactado, nome, email, linkedin, crso, campus) VALUES
+INSERT INTO egressos (id, nomeCompactado, nome, email, linkedin, curso, campus) VALUES
 ('20062370361', 'Aline Donato', 'Aline Donato Carreiro',  '', 'https://www.linkedin.com/in/alinedonato/', 'cstsi', 'ifpb-jp');
 ('20052370067', 'Alana Morais', 'Alana Marques de Morais',  'alana_mm@hotmail.com', 'https://www.linkedin.com/in/alana-morais-b6b0a195/', 'cstsi', 'ifpb-jp');
 ('20052370342', 'Alisson Sena', 'Alisson Alcoforado Sena de Lima', 'alisson.sena@gmail.com', 'https://www.linkedin.com/in/alisson-sena/', 'cstsi', 'ifpb-jp');
