b658a896fd041766aa6900cea128fb82f3591aae
dynnah <dynnah_hanna@hotmail.com>
2019-05-28 22:23:25 -0300

    Update auth.php

--- src/auth.php
+++ src/auth.php
@@ -4,28 +4,28 @@ session_start();
 $login = $_POST['login'] ?? null;
 $password = $_POST['password'] ?? null;
 
-if(authenticateFake($login, $password)){
+if(authenticateUser($login, $password)){
   $_SESSION['auth'] = true;
   header('Location: home.php');
 } else {
   //header('Location: login.html');
 }
 
-function authenticateFake($user, $pass){
+function authenticateUser($user, $pass){
   try{
   $pdo = new PDO("mysql:host=mysql;dbname=prova;port=3306", 'root', '123');
   $sql = 'select * from usuarios where login= :user and senha = :pass';
   $result = $pdo->prepare($sql);
-
+    
   $result->bindParam(":user", $user);
   $result->bindParam(":pass", $pass);
   $result->execute();
-  var_dump($result->fetchAll());
+  return true;
+  //var_dump($result->fetchAll());
   }
-
+  
   catch(Exception $e){
     echo $e->getMessage();
   }
-
-
+  
 }
